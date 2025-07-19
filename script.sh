cd4e8af0d5393efc16bd38230e5bb413974b076b
ntrop <80453846+ntrop6@users.noreply.github.com>
2025-07-19 15:07:35 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+890511#!/bin/bash
 
 echo "Installing packages..."
-if yay -Syyu --noconfirm fish vesktop hyprland-git hyprshot fastfetch grim slurp hyprpolkitagent-git spotify spicetify-cli betterbird-bin keepassxc steam waybar nautilus hyprpaper-git ghostty ttf-jetbrains-mono-nerd; then
-    echo "Packages installed successfully. Proceeding with dotfiles configuration..."
+if yay -Syyu --noconfirm fish vesktop hyprland hyprshot fastfetch grim slurp hyprpolkitagent spotify spicetify-cli betterbird-bin keepassxc steam waybar nautilus hyprpaper890511 ghostty ttf-jetbrains-mono-nerd; then
+    echo "ok"
     cd ~/ || exit
     sudo mkdir -p clones
     cd clones
@@ -12,18 +12,18 @@ if yay -Syyu --noconfirm fish vesktop hyprland-git hyprshot fastfetch grim slurp
     echo "copied succesfully?"
 else
     echo
-    read -p "uh oh, something seems to have gone wrong. Continue anyway? (Y/N) " -n 1 -r
+    read -p "Error Continue anyway? (Y/N) " -n 1 -r
     echo
 
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "Continuing with dotfiles configuration despite package install failure..."
+        echo "Continuing..."
         cd ~/ || exit
         sudo mkdir -p clones
         cd clones/
         sudo rm -rf dotfiles
         sudo git clone https://github.com/ntrop6/dotfiles.git && \
         sudo cp -rf ~/clones/dotfiles/* ~/.config/
-        echo "copied succesfully"
+        echo "ok"
     elif [[ $REPLY =~ ^[Nn]$ ]]; then
         echo "ok go fix your shit."
         exit
