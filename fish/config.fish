b4f36321adbdf9f74b9b49fa80f3ac46c24bc211
ntrop <80453846+ntrop6@users.noreply.github.com>
2025-10-17 16:44:27 +0000

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fastfetch
+fastfetch --logo /home/ntrop/ASCII
+alias sudo='sudo-rs'
+alias sudoedit='sudo-rs rnano'
+alias setimg='sh /home/ntrop/.config/setimage.sh'
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
